554ad787ae23c2696a6f06048c1eb932e059f692
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-10-11 16:11:51 -0700

    commit tree 219bb6cb290ebe8619fced1a893668de794969cf

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,22 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    if params[:name]
+      filtered_students = students.filter do |student|
+        student.first_name.downcase == params[:name] || student.last_name.downcase == params[:name]
+      end
+  
+      render json: filtered_students
+    else
+      render json: students
+    end
+    
+    
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,5 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+
 end
